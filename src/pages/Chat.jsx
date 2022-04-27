d492bc2f850988934a702d90ac9488c26270b039
lnuvy <hangeulgod@naver.com>
2022-04-27 23:51:44 +0900

    유튜브 소켓에서 리덕스 구축해보려고함 (잘못되면 여기로 돌아오기)

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,7 +1,80 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import Contacts from "../components/Contacts";
+import { allUsersRoute } from "../utils/APIRoutes";
 
 function Chat() {
-  return <div>Chat</div>;
+  const navigate = useNavigate();
+  const [contacts, setContacts] = useState([]);
+  const [currentUser, setCurrentUser] = useState(undefined);
+  const [currentChat, setCurrentChat] = useState(undefined);
+
+  useEffect(() => {
+    if (!localStorage.getItem("chat-app-user")) {
+      navigate("/login");
+    } else {
+      setCurrentUser(JSON.parse(localStorage.getItem("chat-app-user")));
+    }
+  }, []);
+
+  useEffect(() => {
+    async function fetchData() {
+      if (currentUser) {
+        if (currentUser.isAvatarImageSet) {
+          console.log(`${allUsersRoute}/${currentUser._id}`);
+          const { data } = await axios.get(
+            `${allUsersRoute}/${currentUser._id}`
+          );
+          setContacts(data);
+        } else {
+          navigate("/setAvatar");
+        }
+      }
+    }
+    fetchData();
+  }, [currentUser]);
+
+  const handleChatChange = (chat) => {
+    setCurrentChat(chat);
+  };
+
+  return (
+    <>
+      <Conatiner>
+        <div className="container">
+          <Contacts
+            contacts={contacts}
+            currentUser={currentUser}
+            changeChat={handleChatChange}
+          />
+        </div>
+      </Conatiner>
+    </>
+  );
 }
+
+const Conatiner = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  background-color: #131324;
+  .container {
+    height: 85vh;
+    width: 85vw;
+    background-color: #00000076;
+    display: grid;
+    grid-template-columns: 25% 75%;
+
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
+      grid-template-columns: 35% 65%;
+    }
+  }
+`;
 
 export default Chat;
